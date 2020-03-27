bdc99b38789e35bc21be85083e4a039492af37cc
ghjinlei <767508270@qq.com>
2020-03-27 11:07:18 +0800

    修改logger编码风格

--- script/lualib/common/logger.lua
+++ script/lualib/common/logger.lua
@@ -8,8 +8,8 @@ Description :
 local skynet = require "skynet"
 local config_system = require "config_system"
 
-local LogLevel = config_system.log.level
-local LogLevelForConsole = config_system.log.level_for_console
+local loglevel = config_system.log.level
+local loglevel_for_console = config_system.log.level_for_console
 
 local loglevel_debug = 1
 local loglevel_info = 2
@@ -17,52 +17,52 @@ local loglevel_warn = 3
 local loglevel_error = 4
 local loglevel_fatal = 5
 
-local LogLevelPrefixMap = {
+local loglevel_prefix_map = {
 	[loglevel_debug] = "\27[0m",
 	[loglevel_info] = "\27[32m",
 	[loglevel_warn] = "\27[33m",
 	[loglevel_error] = "\27[31m",
 	[loglevel_fatal] = "\27[36m",
 }
-local LogLevelTagMap = {
+local loglevel_tag_map = {
 	[loglevel_debug] = "[debug]",
 	[loglevel_info] = "[info]",
 	[loglevel_warn] = "[warn]",
 	[loglevel_error] = "[error]",
 	[loglevel_fatal] = "[fatal]",
 }
-local LogSuffix = "\27[0m"
+local log_suffix = "\27[0m"
 
-local LogTimeFormat = config_system.log.time_format or "%Y%m%d %H:%M:%S"
-local function genLogHeader(timeNowMilli, address)
-	local sec, decimals = math.modf(timeNowMilli)
-	local timeStr = os.date(LogTimeFormat, sec)
+local log_time_format = config_system.log.time_format or "%Y%m%d %H:%M:%S"
+local function gen_log_header(time_now_10ms, address)
+	local sec, decimals = math.modf(time_now_10ms)
+	local time_str = os.date(log_time_format, sec)
 	local tm10ms = math.floor(decimals * 100)
-	return string.format("[%s.%02d][:%08d]", timeStr, tm10ms, address)
+	return string.format("[%s.%02d][:%08d]", time_str, tm10ms, address)
 end
 
-local function printConsole(level, msg)
-	local prefix = LogLevelPrefixMap[level]
-	local levelTag = LogLevelTagMap[level]
-	local logHeader = genLogHeader(skynet.time(), skynet.self())
-	msg = prefix .. logHeader .. levelTag .. msg .. LogSuffix
+local function print_console(level, msg)
+	local prefix = loglevel_prefix_map[level]
+	local level_tag = loglevel_tag_map[level]
+	local log_header = gen_log_header(skynet.time(), skynet.self())
+	msg = prefix .. log_header .. level_tag .. msg .. log_suffix
 	io.write(msg .. '\n')
 end
 
-local function sendLogger(level, msg)
-	local levelTag = LogLevelTagMap[level]
-	msg = levelTag .. msg
+local function send_logger(level, msg)
+	local level_tag = loglevel_tag_map[level]
+	msg = level_tag .. msg
 	skynet.error(msg)
 end
 
 local function write(level, msg)
 	if not (level and msg) then return end
 
-	if level >= LogLevelForConsole then
-		printConsole(level, msg)
+	if level >= loglevel_for_console then
+		print_console(level, msg)
 	end
-	if level >= LogLevel then
-		sendLogger(level, msg)
+	if level >= loglevel then
+		send_logger(level, msg)
 	end
 end
 --]]
